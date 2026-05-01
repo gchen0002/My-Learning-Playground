30d1dacd81ebb716a4e1a3eff4bbdfc1c2687aac
Guanying Chen <xnohohsd1@gmail.com>
2026-04-30 17:09:44 -0700

    fix: support branch pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/My-Learning-Playground/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: 'src/main.tsx',
+        index: 'index.html',
+      },
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/app.js',
+      },
+    },
+  },
   test: {
     environment: 'jsdom',
   },
